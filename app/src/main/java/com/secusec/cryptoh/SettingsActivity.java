9d9034e56a2c2b6ad09622e9a052cebfc41220ae
fuxibut <anderle98@gmail.com>
2019-07-19 23:52:40 +0200

    - removed unused namespace declaration - corrected SettingsActivity - added XML for the "Action after encryption" setting

--- app/src/main/java/com/secusec/cryptoh/SettingsActivity.java
+++ app/src/main/java/com/secusec/cryptoh/SettingsActivity.java
@@ -2,13 +2,30 @@ package com.secusec.cryptoh;
 
 import android.os.Bundle;
 
-public class SettingsActivity extends OptionsMenu {
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
     }
 }
